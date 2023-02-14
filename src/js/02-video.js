3581bdcf4c84f5364aa84e5352c928bec0549e9e
Oleksii Fedorenko <fedorenkolesha@gmail.com>
2023-02-15 00:20:28 +0200

    Add 2nt task solution

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+
+player.on('timeupdate', throttle(writeCurrentTime, 1000));
+
+function writeCurrentTime({ duration, seconds }) {
+  localStorage.setItem(CURRENT_TIME, seconds);
+  if (seconds === duration) localStorage.removeItem(CURRENT_TIME);
+}
