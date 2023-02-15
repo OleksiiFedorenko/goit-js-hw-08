5a7195105fb38b4423cf13e2120d0cbc840a9862
Oleksii Fedorenko <fedorenkolesha@gmail.com>
2023-02-15 23:15:21 +0200

    Add 3rd task solution

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,15 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+const iframeRef = document.querySelector('iframe');
+const player = new Player(iframeRef);
 const CURRENT_TIME = 'videoplayer-current-time';
 
 player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
 
-player.on('timeupdate', throttle(writeCurrentTime, 1000));
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
 
-function writeCurrentTime({ duration, seconds }) {
+function saveCurrentTime({ duration, seconds }) {
   localStorage.setItem(CURRENT_TIME, seconds);
   if (seconds === duration) localStorage.removeItem(CURRENT_TIME);
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const FORM_DATA = 'feedback-form-state';
+let currentData = {};
+
+addSavedData();
+
+formRef.addEventListener('input', throttle(saveData, 500));
+formRef.addEventListener('submit', onSubmit);
+
+function saveData(evt) {
+  currentData[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(FORM_DATA, JSON.stringify(currentData));
+  if (!currentData.email && !currentData.message)
+    localStorage.removeItem(FORM_DATA);
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(currentData);
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(FORM_DATA);
+}
+
+function addSavedData() {
+  const formData = localStorage.getItem(FORM_DATA);
+
+  if (formData) {
+    currentData = JSON.parse(formData);
+    formRef.email.value = currentData.email;
+    formRef.message.value = currentData.message;
+  }
+}
